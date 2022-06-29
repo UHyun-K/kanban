7893e9e783b220ded12781683ce42afa7126d26e
UHyun-K <konglim2@naver.com>
2022-06-29 23:34:39 +0900

    clean draggable

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DraggableCard";
 
 const Wrapper = styled.div`
     display: flex;
@@ -28,12 +29,6 @@ const Board = styled.div`
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 200px;
-`;
-const Card = styled.div`
-    background-color: ${(props) => props.theme.cardColor};
-    padding: 10px 10px;
-    border-radius: 5px;
-    margin-bottom: 5px;
 `;
 
 function App() {
@@ -61,21 +56,11 @@ function App() {
                                 {...magic.droppableProps}
                             >
                                 {toDos.map((toDo, index) => (
-                                    <Draggable
+                                    <DraggableCard
                                         key={toDo}
-                                        draggableId={toDo}
                                         index={index}
-                                    >
-                                        {(magic) => (
-                                            <Card
-                                                ref={magic.innerRef}
-                                                {...magic.draggableProps}
-                                                {...magic.dragHandleProps}
-                                            >
-                                                {toDo}
-                                            </Card>
-                                        )}
-                                    </Draggable>
+                                        toDo={toDo}
+                                    />
                                 ))}
                                 {magic.placeholder}
                             </Board>
---
+++ src/Components/DraggableCard.tsx
@@ -0,0 +1,31 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+
+const Card = styled.div`
+    background-color: ${(props) => props.theme.cardColor};
+    padding: 10px 10px;
+    border-radius: 5px;
+    margin-bottom: 5px;
+`;
+
+interface IDraggableCardProps{
+    toDo:string;
+    index:number;
+}
+function DraggableCard({toDo, index}:IDraggableCardProps) {
+    return (
+        <Draggable key={toDo} draggableId={toDo} index={index}>
+            {(magic) => (
+                <Card
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    {...magic.dragHandleProps}
+                >
+                    {toDo}
+                </Card>
+            )}
+        </Draggable>
+    );
+}
+export default DraggableCard;
