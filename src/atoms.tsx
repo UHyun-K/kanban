ec60f11215383948a805afe389aab3a62e835cb7
UHyun-K <konglim2@naver.com>
2022-07-12 19:56:13 +0900

    newToDo

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -10,7 +10,7 @@ interface IToDoState {
 export const toDoState = atom<IToDoState>({
     key: "toDo",
     default: {
-        "To Do": [{id:1, text:"Hello"}, {id:2, text:"bye"}],
+        "To Do": [],
         Doing: [],
         Done: [],
     },
